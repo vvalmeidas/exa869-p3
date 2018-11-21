b20941eca6be406892284131ab5a77070ba989de
Nadine <nadymarkes@gmail.com>
2018-11-21 20:35:35 -0300

    Update Expressions - Expressions testada

--- Compiler/src/Main.java
+++ Compiler/src/Main.java
@@ -58,20 +58,21 @@ public class Main {
 		tokens.add(new Token("IDENTIFICADOR", "b", 0));*/
 				
 		
-		//a>b!a+b-3++
+		//a > b!a+b-3++
+		// a > b
 		
 		tokens.add(new Token("IDENTIFICADOR", "a", 0));
 		tokens.add(new Token("OPERADOR_RELACIONAL", ">", 0));
 		tokens.add(new Token("IDENTIFICADOR", "b", 0));
-		
-		tokens.add(new Token("OPERADOR_LOGICO", "!", 0));
-		tokens.add(new Token("IDENTIFICADOR", "a", 0));
-		tokens.add(new Token("OPERADOR_ARITMETICO", "+", 0));
-		tokens.add(new Token("IDENTIFICADOR", "b", 0));
-
-		tokens.add(new Token("OPERADOR_ARITMETICO", "-", 0));
-		tokens.add(new Token("NUMERO", "3", 0));
-		tokens.add(new Token("OPERADOR_RELACIONAL", "++", 0));
+//		
+//		tokens.add(new Token("OPERADOR_LOGICO", "!", 0));
+//		tokens.add(new Token("IDENTIFICADOR", "a", 0));
+//		tokens.add(new Token("OPERADOR_ARITMETICO", "+", 0));
+//		tokens.add(new Token("IDENTIFICADOR", "b", 0));
+//
+//		tokens.add(new Token("OPERADOR_ARITMETICO", "-", 0));
+//		tokens.add(new Token("NUMERO", "3", 0));
+//		tokens.add(new Token("OPERADOR_RELACIONAL", "++", 0));
 		
 		
 		TokensFlow.setTokensSet(tokens);
